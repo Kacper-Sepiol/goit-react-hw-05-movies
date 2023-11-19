dbc29ff4ea36f66f41e7546f4ab49fabc1bb1399
Kacper <kac-2008wp.pl@wp.pl>
2023-11-19 13:11:47 +0100

    update

---
+++ src/fetchFilms.js
@@ -0,0 +1,7 @@
+// const key = '493ca07ced65ee69b94ce62a3e3db755';
+
+// export function fetchFilms() {
+
+// }
+
+// do poprawy
---
+++ src/pages/Home.jsx
@@ -0,0 +1,10 @@
+export const Home = () => {
+  const key = '493ca07ced65ee69b94ce62a3e3db755';
+  const url = 'https://api.themoviedb.org';
+
+  fetch(url);
+
+  // strona domowa z list popularnych filmow
+
+  return <h1>Trending Today</h1>;
+};
