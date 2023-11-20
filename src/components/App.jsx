e0066f16b559a3416ab8eafe81135e543ce62f8e
Kacper <kac-2008wp.pl@wp.pl>
2023-11-20 12:20:06 +0100

    update

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,14 +1,49 @@
 import { Link, Routes, Route } from 'react-router-dom';
 import { Home } from '../pages/Home';
+import { useState, useEffect } from 'react';
 
 export const App = () => {
-  return (
-    <div>
-      <Link to="/">Home</Link>
+  const apiKey = '493ca07ced65ee69b94ce62a3e3db755';
+  const apiUrl = `https://api.themoviedb.org/3/trending/all/day?language=en-US&api_key=${apiKey}`;
+  const [films, setFilms] = useState([]);
 
-      <Routes>
-        <Route path="/" element={<Home />} />
-      </Routes>
-    </div>
+  //   const options = {
+  //     method: 'GET',
+  //     headers: {
+  //       accept: 'application/json',
+  //     },
+  //   };
+
+  useEffect(() => {
+    fetch(apiUrl)
+      .then(response => response.json())
+      .then(response => {
+        const filteredFilms = response.results.filter(
+          (film, index) => index !== 20
+        );
+        setFilms(filteredFilms);
+        console.log(filteredFilms);
+      })
+      .catch(err => console.error(err));
+  }, [apiUrl]);
+
+  return (
+    <>
+      <div>
+        <Link to="/">Home</Link>
+
+        <Routes>
+          <Route path="/" element={<Home />} />
+        </Routes>
+
+        <ul>
+          {films.map(film => (
+            <li key={film.id}>
+              <Link to="/movies">{film.title}</Link>
+            </li>
+          ))}
+        </ul>
+      </div>
+    </>
   );
 };
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,36 +1,39 @@
 import { useState, useEffect } from 'react';
 
 export const Home = () => {
-  const apiKey = '493ca07ced65ee69b94ce62a3e3db755';
-  const apiUrl = `https://api.themoviedb.org/3/trending/all/day?language=en-US&api_key=${apiKey}`;
-  const [films, setFilms] = useState([]);
+  // const apiKey = '493ca07ced65ee69b94ce62a3e3db755';
+  // const apiUrl = `https://api.themoviedb.org/3/trending/all/day?language=en-US&api_key=${apiKey}`;
+  // const [films, setFilms] = useState([]);
 
-  //   const options = {
-  //     method: 'GET',
-  //     headers: {
-  //       accept: 'application/json',
-  //     },
-  //   };
+  // //   const options = {
+  // //     method: 'GET',
+  // //     headers: {
+  // //       accept: 'application/json',
+  // //     },
+  // //   };
 
-  useEffect(() => {
-    fetch(apiUrl)
-      .then(response => response.json())
-      .then(response => {
-        setFilms(response.results[0]);
-      })
-      .catch(err => console.error(err));
-  }, [apiUrl]);
-
-  console.log(films);
+  // useEffect(() => {
+  //   fetch(apiUrl)
+  //     .then(response => response.json())
+  //     .then(response => {
+  //       const filteredFilms = response.results.filter(
+  //         (film, index) => index !== 20
+  //       );
+  //       setFilms(filteredFilms);
+  //     })
+  //     .catch(err => console.error(err));
+  // }, [apiUrl]);
 
   // strona domowa z list popularnych filmow
 
   return (
     <div>
       <h1>Trending Today</h1>
-      <ul>
-        <li key={films.key}>{films.results}</li>
-      </ul>
+      {/* <ul>
+        {films.map(film => (
+          <li key={film.id}>{film.title}</li>
+        ))}
+      </ul> */}
     </div>
   );
 };
